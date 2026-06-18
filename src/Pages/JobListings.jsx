d7dd085ac3ae225b754aa700a4aa38651f824cb5
afzytk <afzytk@gmail.com>
2026-06-18 19:52:27 +0530

    Add custom hook and add logo image

--- src/Pages/JobListings.jsx
+++ src/Pages/JobListings.jsx
@@ -1,9 +1,19 @@
-
+import { getJobs } from "../api/apiJobs";
+import { useEffect } from "react";
+import { useSession } from "@clerk/clerk-react";
 
 const JobListing = () => {
-  return (
-    <div>JobListing</div>
-  )
-}
+  const fetchJobs = async () => {
+    const supabaseAccessToken = await session.getToken({
+      template: "supabase",
+    });
+    const data = await getJobs(supabaseAccessToken);
+    console.log(data);
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+  return <div>JobListing</div>;
+};
 
-export default JobListing
+export default JobListing;
---
+++ src/api/apiJobs.js
@@ -0,0 +1,12 @@
+import supabaseClient from "@/utils/supabase";
+
+export async function getJobs(token, { location, company_id, searchQuery }) {
+  const supabase = await SupabaseClient(token);
+  let query = supabase.from("jobs").select("*");
+  const { data, error } = await query;
+  if (error) {
+    console.error("Error fetching Jobs:", error);
+    return null;
+  }
+  return data;
+}
